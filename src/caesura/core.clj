a62c7fa648052ff043a9cb57b4faea5eae16eda9
Ian Truslove <ian.truslove@gmail.com>
2017-04-04 07:58:51 -0600

    WIP

--- src/caesura/core.clj
+++ src/caesura/core.clj
@@ -2,14 +2,40 @@
 
 ;; intended usage
 
-(def cb (new-circuit-breaker {:call-timeout 5000 #_msecs
-                              :reset-timeout 60000 #_msecs
-                              :max-failures 5}))
+{:call-timeout 5000 #_msecs
+ :reset-timeout 60000 #_msecs
+ :max-failures 5}
 
-(with-breaker cb
-  ;; Single sexp
-  (println "hello!"))
+(defn new-circuit-breaker [{:keys [call-timeout reset-timeout max-failures] :as args}]
+  (merge args {:open-since nil}))
+
+(comment
+  
+  (with-breaker cb
+    ;; Single sexp
+    (println "hello!")))
 ;; Returns the now-updated circuit breaker. Would be nice if it returned the
 ;; return value too.
 
 ;; But this won't be super-useful, because of the immutability.
+
+(defn breaker [v]
+  (:breaker v))
+
+(defn err [v]
+  (:err v))
+
+(defn val [v]
+  (:val v))
+
+(defn now []
+  (java.util.Date.))
+
+(defn breaker-open-since [breaker]
+  (:open-since breaker))
+
+(defn millis-since-breaker-opened [breaker]
+  )
+
+(defn run-fn [{:keys [breaker err val]} f]
+  )
--- test/caesura/core_test.clj
+++ test/caesura/core_test.clj
@@ -1,7 +1,40 @@
 (ns caesura.core-test
   (:require [clojure.test :refer :all]
-            [caesura.core :refer :all]))
+            [caesura.core :as cb]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+
+(defn err? [v]
+  (boolean (first v)))
+
+(defn ok? [v]
+  (not (err? v)))
+
+(defn breaker-open? [v]
+  (-> v
+      :breaker
+      :open-since
+      boolean))
+
+(defn breaker-closed? [v]
+  (not (breaker-open? v)))
+
+(deftest test-circuit-breakage
+  (testing "when max-failures is exceeded, the circuit breaker trips"
+    (let [call-count (atom 0)
+          always-throw (fn []
+                         (swap! call-count inc)
+                         (throw (Exception.)))
+          breaker (cb/new-circuit-breaker {:call-timeout 5000 #_msecs
+                                           :reset-timeout 60000 #_msecs
+                                           :max-failures 3})]
+      (let [three-fails (-> {:error nil :value nil :breaker cb}
+                            (cb/run-fn always-throw)
+                            (cb/run-fn always-throw)
+                            (cb/run-fn always-throw))
+            _ (is (breaker-closed? three-fails))
+            four-fails (cb/run-fn three-fails always-throw)
+            _ (is (breaker-open? four-fails))
+            ]
+        )
+      
+      )))
